7a034c085207ed8b599f63896527b79ce153594b
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-26 17:01:12 +0530

    Add project files.

---
+++ StringReverseUsingFunction/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringReverseUsingFunction
+{
+    internal class Class1
+    {
+        public static void reverse()
+        {
+            Console.WriteLine("Enter a String Data:");
+            string data= Console.ReadLine();
+
+            char[] charArr = data.ToCharArray();
+
+            string reverse = "";
+            int length = data.Length - 1;
+
+            while (length >= 0)
+            {
+                reverse += data[length];
+                length--;
+            }
+
+            Console.WriteLine("\nReverse String Will Be: {0} ",reverse);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        
+
+    }
+}
---
+++ StringReverseUsingFunction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringReverseUsingFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.reverse();
+        }
+    }
+}
